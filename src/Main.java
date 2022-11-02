da72537899088cc7c904d5bdaefa884b1c8f82bc
1dina <dinafadel69@gmail.com>
2022-11-02 21:25:50 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        int index =s.length(),counter=0;
+        if (index %2==0){
+        for (int i=0;i <index/2;i++){
+            if(s.charAt(i) !=s.charAt(index-i-1)){
+                counter++;
+            }
+    }}else{
+            for (int i=0;i <(index-1)/2;i++){
+                if(s.charAt(i) !=s.charAt(index-i-1)){
+                    counter++;
+                }
+            }
+
+        }
+        if (counter >1){
+            System.out.println("No");
+        }else{
+            System.out.println("Yes");
+        }
+
+}}
