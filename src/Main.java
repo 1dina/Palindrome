c5d06d6ddc37c3039612f346ce8af54fc57deffc
1dina <dinafadel69@gmail.com>
2022-11-02 22:52:14 +0200

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,30 @@ public class Main {
         Scanner in = new Scanner(System.in);
         String s = in.next();
         int index =s.length(),counter=0;
+
         if (index %2==0){
         for (int i=0;i <index/2;i++){
             if(s.charAt(i) !=s.charAt(index-i-1)){
                 counter++;
-            }
-    }}else{
+            }}
+            if (counter >1 || counter==0)
+                System.out.println("No");
+            else
+                System.out.println("Yes");
+
+    }else{
             for (int i=0;i <(index-1)/2;i++){
                 if(s.charAt(i) !=s.charAt(index-i-1)){
                     counter++;
-                }
+                }}
+            if (counter >1)
+                System.out.println("No");
+            else
+                System.out.println("Yes");
+
+
             }
 
-        }
-        if (counter >1){
-            System.out.println("No");
-        }else{
-            System.out.println("Yes");
-        }
+
 
 }}
